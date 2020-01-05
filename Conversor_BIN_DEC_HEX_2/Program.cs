cf0e694fa3232226618acfc7edf181fb45b922da
Inhanga <nakappa1@hotmail.com>
2020-01-05 00:52:55 -0300

    Projeto inciado

---
+++ Conversor_BIN_DEC_HEX_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conversor_BIN_DEC_HEX_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
