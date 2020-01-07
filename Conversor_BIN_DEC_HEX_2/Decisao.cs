243ab17a5d588bf5a0062c7635698bfb0474305b
Inhanga <nakappa1@hotmail.com>
2020-01-07 04:22:35 -0300

    Implementando a classe Verificador e alterando a Decisao

--- Conversor_BIN_DEC_HEX_2/Decisao.cs
+++ Conversor_BIN_DEC_HEX_2/Decisao.cs
@@ -5,153 +5,163 @@ namespace Conversor_BIN_DEC_HEX_2
 {
     class Decisao
     {
-        public static void ChamandoConvBinHex(int escolha, string digito)
+        public static void ChamandoConvBinHex(string digito, int escolha)
         {
-            bool controle = false;
-            while (!controle)
-            {
-                foreach (char a in digito) if (a != '0' && a != '1' && a != '2' && a != '3' && a != '4' && a != '5' && a != '6' && a != '7' && a != '8' && a != '9') controle = true;
-
-                if (controle)
-                {
-                    Console.WriteLine("FAVOR DIGITAR ALGARISMOS ENTRE 0 À 9!!!\n");
-                    Console.Write("Digite um número decimal: ");
-                    digito = Console.ReadLine().ToUpper();
-                    Console.WriteLine();
-                    Console.WriteLine("Número digitado foi: " + digito);
-                    controle = false;
-                }
-
-                else
-                {
-                    //Conversores conv = new Conversores();
-                    //string conversor = conv.ConvBinHex(digito, escolha);
-                    string conversor = Conversores.ConvBinHex(digito, escolha);
-                    if (escolha == 3) Console.WriteLine("Convertido em hexadecimal: " + conversor);
-                    else Console.WriteLine("Convertido em binário: " + conversor);
-                    controle = true;
-                }
-            }
+            string conversor = Conversores.ConvBinHex(digito, escolha);
+            if (escolha == 3) Console.WriteLine("Convertido em hexadecimal: " + conversor);
+            else Console.WriteLine("Convertido em binário: " + conversor);
         }
 
-        public static void ChamandoConvDec(int escolha, string digito)
+        public static void ChamandoConvDec(string digito, int escolha)
         {
-            bool controle = false;
-
-            while (!controle)
-            {
-                if (escolha == 1)
-                    foreach (char a in digito)
-                        if (a != '0' && a != '1') controle = true;
-
-                if (escolha == 3)
-                    foreach (char a in digito)
-                        if (a != '0' && a != '1' && a != '2' && a != '3' && a != '4' && a != '5' && a != '6' && a != '7' && a != '8' && a != '9' && a != 'A' && a != 'B' && a != 'C' && a != 'D' && a != 'E' && a != 'F') controle = true;
-
-                if (controle)
-                {
-                    Console.WriteLine("FAVOR DIGITAR ALGARISMOS ENTRE 0 E 1!!!");
-
-                    if (escolha == 1) Console.Write("Digite um número binário: ");
-
-                    if (escolha == 3) Console.Write("Digite um número hexadecimal: ");
-
-                    digito = Console.ReadLine();
-                    Console.WriteLine();
-                    Console.WriteLine("Número digitado foi: " + digito);
-                    controle = false;
-                }
-
-                else
-                {
-                    //Conversores conv = new Conversores();
-                    //string conversor = conv.ConvDec(digito, escolha);
-                    string conversor = Conversores.ConvBinHex(digito, escolha);
-                    Console.WriteLine(conversor);
-                    controle = true;
-
-                }
-            }
+            string conversor = Conversores.ConvDec(digito, escolha);
+            Console.WriteLine("Convertido em decimal: " + conversor);
         }
 
-        public void Escolher()
+        public static void Escolher()
         {
-            Console.WriteLine("CONVERSOR NUMÉRICO\n");
-            Console.Write("Escolha:" +
-                "\n1 - Para converter BINÁRIO em DECIMAL / HEXADECIMAL" +
-                "\n2 - Para converter DECIMAL em BINÁRIO / HEXADECIMAL" +
-                "\n3 - Para converter HEXADECIMAL em DECIMAL / BINÁRIO" +
-                "\n4 - Para SAIR: ");
+            Conversores conv = new Conversores(); ;
 
             bool controle = false;
-
-            while (!controle)
+            try
             {
-                Conversores conv = new Conversores { escolha = int.Parse(Console.ReadLine()) };
-                if (conv.escolha < 1 || conv.escolha > 4)
+
+                while (!controle)
                 {
-                    Console.Write("\nFAVOR DIGITAR UM VALOR ENTRE 1 À 4!!!" +
-                        "\nQual a sua escolha?" +
-                        "\n1 - (BINÁRIO / DECIMAL & HEXADECIMAL)" +
-                        "\n2 - (DECIMAL / BINÁRIO & HEXADECIMAL)" +
-                        "\n3 - (HEXADECIMAL / DECIMAL & BINÁRIO)" +
-                        "\n4 - (SAIR): ");
-                }
-
-                if (conv.escolha == 1)
-                {
-                    Console.Write("\nDigite um número binário: ");
-                    string digito = Console.ReadLine().ToUpper();
-                    Console.WriteLine($"\nNúmero digitado foi: {digito}");
-
-                    ChamandoConvDec(conv.escolha, digito);
-
                     Console.Write("\nEscolha:" +
-                "\n1 - Para converter BINÁRIO em DECIMAL / HEXADECIMAL" +
-                "\n2 - Para converter DECIMAL em BINÁRIO / HEXADECIMAL" +
-                "\n3 - Para converter HEXADECIMAL em DECIMAL / BINÁRIO" +
-                "\n4 - Para SAIR: ");
+                        "\n1 - Para converter BINÁRIO em DECIMAL / HEXADECIMAL" +
+                        "\n2 - Para converter DECIMAL em BINÁRIO / HEXADECIMAL" +
+                        "\n3 - Para converter HEXADECIMAL em DECIMAL / BINÁRIO" +
+                        "\n4 - Para SAIR: ");
+
+                    conv.escolha = int.Parse(Console.ReadLine());
+                    if (conv.escolha < 1 || conv.escolha > 4)
+                    {
+                        ConsoleColor aux = Console.BackgroundColor;
+                        Console.BackgroundColor = ConsoleColor.DarkRed;
+                        ConsoleColor aux2 = Console.ForegroundColor;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("\nFAVOR DIGITAR UM VALOR ENTRE 1 À 4!!!");
+                        Console.BackgroundColor = aux;
+                        Console.ForegroundColor = aux2;
+
+                        Console.Write("\nQual a sua escolha?" +
+                            "\n1 - (BINÁRIO / DECIMAL & HEXADECIMAL)" +
+                            "\n2 - (DECIMAL / BINÁRIO & HEXADECIMAL)" +
+                            "\n3 - (HEXADECIMAL / DECIMAL & BINÁRIO)" +
+                            "\n4 - (SAIR): ");
+                    }
+
+                    if (conv.escolha == 1)
+                    {
+                        Console.Write("\nDigite um número binario: ");
+                        string digito = Console.ReadLine();
+
+                        //controle = 0;
+
+                        while (!controle)
+                        {
+                            string erro = Verificador.Verificar(digito, conv.escolha);
+
+                            if (erro == "ok")
+                            {
+                                digito = Console.ReadLine();
+                                controle = false;
+                            }
+
+                            else
+                            {
+                                Console.WriteLine($"\nNúmero digitado foi: {digito}");
+                                ChamandoConvDec(digito, conv.escolha);
+                                ChamandoConvBinHex(Conversores.ConvDec(digito, conv.escolha), conv.escolha = 3);
+                                conv.escolha = 0;
+                                controle = true;
+                            }
+                        }
+
+                        controle = false;
+                    }
+
+                    if (conv.escolha == 2)
+                    {
+                        Console.Write("\nDigite um número decimal: ");
+                        string digito = Console.ReadLine();
+
+                        //controle = 0;
+
+                        while (!controle)
+                        {
+                            string erro = Verificador.Verificar(digito, conv.escolha);
+
+                            if (erro == "ok")
+                            {
+                                digito = Console.ReadLine();
+                                controle = false;
+                            }
+
+                            else
+                            {
+                                Console.WriteLine($"\nNúmero digitado foi: {digito}");
+                                ChamandoConvBinHex(digito, conv.escolha);
+                                ChamandoConvBinHex(digito, conv.escolha = 3);
+                                conv.escolha = 0;
+                                controle = true;
+                            }
+                        }
+
+                        controle = false;
+                    }
+
+                    if (conv.escolha == 3)
+                    {
+                        Console.Write("\nDigite um número hexadecimal: ");
+                        string digito = Console.ReadLine().ToUpper();
+
+                        //controle = 0;
+
+                        while (!controle)
+                        {
+                            string erro = Verificador.Verificar(digito, conv.escolha);
+
+                            if (erro == "ok")
+                            {
+                                digito = Console.ReadLine().ToUpper();
+                                controle = false;
+                            }
+
+                            else
+                            {
+                                Console.WriteLine($"\nNúmero digitado foi: {digito}");
+                                ChamandoConvDec(digito, conv.escolha);
+                                ChamandoConvBinHex(Conversores.ConvDec(digito, conv.escolha), conv.escolha = 1);
+                                conv.escolha = 0;
+                                controle = true;
+                            }
+                        }
+
+                        controle = false;
+                    }
+
+                    if (conv.escolha == 4)
+                    {
+                        Console.WriteLine("\nADEUS!!!");
+                        controle = true;
+                    }
                 }
+            }
 
-                if (conv.escolha == 2)
-                {
-                    Console.Write("\nDigite um número decimal: ");
-                    string digito = Console.ReadLine().ToUpper();
-                    Console.WriteLine($"\nNúmero digitado foi: {digito}");
+            catch (Exception)
+            {
+                //Console.WriteLine(e.Message);
+                ConsoleColor aux = Console.BackgroundColor;
+                Console.BackgroundColor = ConsoleColor.DarkRed;
+                ConsoleColor aux2 = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\nFAVOR DIGITAR UM VALOR ENTRE 1 À 4!!!");
+                Console.BackgroundColor = aux;
+                Console.ForegroundColor = aux2;
 
-                    ChamandoConvBinHex(conv.escolha, digito);
-                    ChamandoConvBinHex(conv.escolha = 3, digito);
-                    conv.escolha = 0;
-
-                    Console.Write("\nEscolha:" +
-                "\n1 - Para converter BINÁRIO em DECIMAL / HEXADECIMAL" +
-                "\n2 - Para converter DECIMAL em BINÁRIO / HEXADECIMAL" +
-                "\n3 - Para converter HEXADECIMAL em DECIMAL / BINÁRIO" +
-                "\n4 - Para SAIR: ");
-                }
-
-                if (conv.escolha == 3)
-                {
-                    Console.Write("\nDigite um número hexadecimal: ");
-                    string digito = Console.ReadLine().ToUpper();
-                    Console.WriteLine($"\nNúmero digitado foi: {digito}");
-                    ChamandoConvDec(conv.escolha, digito);
-                    //conv.ConvDec(digito, conv.escolha);
-                    //dcd.x = conv.resultado;
-                    //Console.WriteLine(dcd.x);
-
-                    Console.Write("\nEscolha:" +
-                "\n1 - Para converter BINÁRIO em DECIMAL / HEXADECIMAL" +
-                "\n2 - Para converter DECIMAL em BINÁRIO / HEXADECIMAL" +
-                "\n3 - Para converter HEXADECIMAL em DECIMAL / BINÁRIO" +
-                "\n4 - Para SAIR: ");
-                }
-
-                if (conv.escolha == 4)
-                {
-                    Console.WriteLine("ADEUS!!!");
-                    controle = true;
-                }
+                Escolher();
             }
         }
     }
---
+++ Conversor_BIN_DEC_HEX_2/Verificador.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Conversor_BIN_DEC_HEX_2
+{
+    class Verificador
+    {
+        public static string Verificar(string numero, int escolha)
+        {
+            string erro = "nenhum";
+            bool controlador = false;
+
+            if (escolha == 1)
+            {
+                foreach (char a in numero)
+                {
+                    if (a != '0' && a != '1')
+                    {
+                        controlador = true;
+                    }
+                }
+
+                if (controlador)
+                {
+                    ConsoleColor aux = Console.BackgroundColor;
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    ConsoleColor aux2 = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("FAVOR DIGITAR ALGARISMOS ENTRE 0 E 1!!!\n");
+                    Console.BackgroundColor = aux;
+                    Console.ForegroundColor = aux2;
+                    Console.Write("Digite um número binário: ");
+                    erro = "ok";
+                }
+            }
+
+            if (escolha == 2)
+            {
+                foreach (char a in numero)
+                {
+                    if (a != '0' && a != '1' && a != '2' && a != '3' && a != '4' && a != '5' && a != '6' && a != '7' && a != '8' && a != '9')
+                    {
+                        controlador = true;
+                    }
+                }
+
+                if (controlador)
+                {
+                    ConsoleColor aux = Console.BackgroundColor;
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    ConsoleColor aux2 = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("FAVOR DIGITAR ALGARISMOS ENTRE 0 À 9!!!\n");
+                    Console.BackgroundColor = aux;
+                    Console.ForegroundColor = aux2;
+                    Console.Write("Digite um número decimal: ");
+                    erro = "ok";
+                }
+            }
+
+            if (escolha == 3)
+            {
+                foreach (char a in numero)
+                {
+                    if (a != '0' && a != '1' && a != '2' && a != '3' && a != '4' && a != '5' && a != '6' && a != '7' && a != '8' && a != '9' && a != 'A' && a != 'B' && a != 'C' && a != 'D' && a != 'E' && a != 'F')
+                    {
+                        controlador = true;
+                    }
+                }
+
+                if (controlador)
+                {
+                    ConsoleColor aux = Console.BackgroundColor;
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    ConsoleColor aux2 = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("FAVOR DIGITAR ALGARISMOS ENTRE 0 À 9 E OU A ATÉ F!!!\n");
+                    Console.BackgroundColor = aux;
+                    Console.ForegroundColor = aux2;
+                    Console.Write("Digite um número hexadecimal: ");
+                    erro = "ok";
+                }
+            }
+
+            return erro;
+        }
+    }
+}
